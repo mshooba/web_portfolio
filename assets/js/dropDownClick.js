996b4b68dc6b2e962973c1420dcdd93726e543f1
matthew sciubba <matthewsciubba@matthews-MacBook-Air-2.local>
2022-09-06 20:57:05 -0400

    changing layout

--- assets/js/dropDownClick.js
+++
@@ -1,9 +0,0 @@
-function dropdownMenu() {
-    var x = document.getElementById("dropdownClick");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-        /*change topnav to topnav responsive*/
-    } else {
-        x.className === "topnav";
-    }
-}
